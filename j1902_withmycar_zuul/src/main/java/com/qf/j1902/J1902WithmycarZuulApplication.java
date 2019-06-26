0f6842438f3cdc9d14d2196ebcc49324c60d3272
git <1554148254@qq.com>
2019-06-26 19:27:44 +0800

    Initial commit

---
+++ j1902_withmycar_zuul/src/main/java/com/qf/j1902/J1902WithmycarZuulApplication.java
@@ -0,0 +1,13 @@
+package com.qf.j1902;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J1902WithmycarZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(J1902WithmycarZuulApplication.class, args);
+    }
+
+}
