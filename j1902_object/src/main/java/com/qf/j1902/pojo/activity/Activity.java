407aff0fd798a72e09e09beeb489929cce493ed3
git <1554148254@qq.com>
2019-06-27 16:55:34 +0800

    第一次

---
+++ j1902_object/src/main/java/com/qf/j1902/pojo/activity/Activity.java
@@ -0,0 +1,42 @@
+package com.qf.j1902.pojo.activity;
+
+import lombok.Data;
+
+@Data
+public class Activity {
+    private Integer id;
+    private String title;
+    private Integer categoryId;
+    private String url;
+    private Integer states;
+    private String summary;
+    private String description;
+    private String link;
+    private Integer beginTime;
+    private Integer endTime;
+    private Integer channelTop;
+    private Integer homeTop;
+    private Integer pvCount;
+    private Integer uvCount;
+    private Integer upCount;
+    private Integer favCount;
+    private Integer publishState;
+    private Integer createUid;
+    private Integer createTime;
+    private String author;
+    private String unpublishUname;
+    private String unpublishTime;
+    private String seoKeywords;
+    private String sdkPath;
+    private Integer likeCount;
+    private Integer shareCount;
+    private Integer viewCount;
+    private String seoDescription;
+    private String tags;
+    private String coverPic;
+    private String cars;
+    private Integer sdkId;
+    private String sdkTitle;
+    private String dealer;
+
+}
