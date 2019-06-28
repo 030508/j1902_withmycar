cc9aab85b334d5d2be6f32be4d630eec3e9ea990
git <1554148254@qq.com>
2019-06-28 19:33:42 +0800

    pojo

--- j1902_withmycar_dao/src/main/java/com/qf/j1902/mapper/AdminUserMapper.java
+++ j1902_withmycar_dao/src/main/java/com/qf/j1902/mapper/AdminUserMapper.java
@@ -8,10 +8,17 @@ import java.util.List;
 
 @Mapper
 public interface AdminUserMapper {
-    List<AdminUser> findAll(); //查询所有用户
+    List<AdminUser> findAll(@Param("startIndex") Integer startIndex,@Param("endIndex") Integer endIndex); //查询所有用户
     AdminUser findOneByName(String username);//根据用户名查询
     void deleteOneByName(String username);//根据用户名删除用户
     void updetaOneByName(@Param("phone") String phone,@Param("email") String email,@Param("username") String username);
-    void addByAdminUser(AdminUser adminUser);//添加AdminUser
+    void updetaPasswordByName(@Param("password") String password,@Param("username") String username); //修改密码
+    void addByAdminUser(@Param("adminUser") AdminUser adminUser);//添加AdminUser
+
+    void updatestatusByName(Integer status, String username); //修改用户状态
+
+
+    Integer findAllCount();   //查询总记录数
+
 
 }
